d094a81824f24db35b5d6b27733bd7574610f17f
Jose-M-Otero <ieagle71@gmail.com>
2022-11-10 18:14:40 -0600

    More updates NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-// TODO: Add a comment explaining what this import statement is doing
-import HelloReact from './components/HelloReact';
+// import React from "react";
+// // TODO: Add a comment explaining what this import statement is doing
+// // import HelloReact from "./components/HelloReact";
+// import Navbar from "./components/Navbar";
 
-// TODO: Add a comment explaining the purpose of the App component
+import Navbar from './components/Navbar';
+
+// // TODO: Add a comment explaining what this function is doing
+// // This function is a functional component that helps us split the UI into distinct parts.
+// // In this case, we are returning another component, <HelloReact/> from it.
 function App() {
-  return <HelloReact />;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/HelloReact.js
+++ src/components/HelloReact.js
@@ -1,11 +1,18 @@
+// Type "rafce" to create a React Arrow Function Component with ES7 moudle syste (ES7+React/Redux/React-Native snippets)
+
 import React from "react";
 
 // TODO: Add a comment explaining what a react component is
+// Components are responsible for rendering some part of an application's UI.
+// Components let us split the UI into independent, reusable pieces, and also think about each piece in isolation.
 function HelloReact() {
   const text = "some text";
 
   // TODO: Add a comment explaining what JSX is and the significance of the curly braces
-  return <p>Hello World! Here is {text}</p>;
+  // JSX is a syntax extension to JavaScript that looks like HTML.
+  // JSX is compiled by something called "Babel" into widely supported ES5 syntax.
+  // The curly braces allow us to pass values and expressions into our view.
+  return <h1>Hello World! Here is {text}</h1>;
 }
 
 export default HelloReact;
